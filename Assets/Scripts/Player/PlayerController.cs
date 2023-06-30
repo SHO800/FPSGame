b2ad54ad10d23af4ecc162d849916c68d2169ca2
SHO800 <fg582323@docomo.ne.jp>
2023-06-30 17:14:19 +0900

    スクリプトの整理とスコープの修正

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
-using UnityEngine.UIElements;
 using Cursor = UnityEngine.Cursor;
 
 public class PlayerController : MonoBehaviour
@@ -13,82 +9,69 @@ public class PlayerController : MonoBehaviour
     public float mouseSensibilityHorizontal = 1f;
     public float mouseSensibilityVertical = 0.5f;
 
-    private Transform headBone;
-    private Rigidbody rb;
-    private Animator anim;
-    private float speed;
-    private Camera mainCamera;
-    private Vector3 cameraForward;
-    private Vector3 moveX;
-    private Vector3 moveZ;
-    private Vector3 moveDirection = Vector3.zero;
-    private Transform headHorizontal;
-    private bool isOnGround;
-    private float localHeadAngle;
+    private Transform _headBone;
+    private Rigidbody _rb;
+    private Camera _mainCamera;
+    private Animator _anim;
+    private bool _isOnGround;
 
 
     private void Start()
     {
-        rb = transform.GetComponent<Rigidbody>();
-        mainCamera = Camera.main;
-        
+        _rb = transform.GetComponent<Rigidbody>();
+        _mainCamera = Camera.main;
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
-        headBone = GameObject.Find("Root/Hips/Spine/Spine1/Neck/Head").transform;
+        _headBone = GameObject.Find("Root/Hips/Spine/Spine1/Neck/Head").transform;
     }
-    
+
     private void Update()
     {
         // 移動処理
-        speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : normalSpeed; // 移動速度を取得
-        cameraForward = Vector3.Scale(
-            mainCamera.transform.forward, 
+        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : normalSpeed; // 移動速度を取得
+        Vector3 cameraForward = Vector3.Scale(
+            _mainCamera.transform.forward,
             new Vector3(1, 0, 1)
         ).normalized; // カメラの正面方向のベクトルを取得
-        
+
         // 移動キーの入力から移動ベクトルを取得
-        moveZ = cameraForward * (Input.GetAxis("Vertical") * 1); // 前後(カメラ基準)
-        moveX = mainCamera.transform.right * (Input.GetAxis("Horizontal") * 1); // 左右(カメラ基準)
-        moveDirection = moveX + moveZ;
+        Vector3 moveZ = cameraForward * (Input.GetAxis("Vertical") * 1); // 前後(カメラ基準)
+        Vector3 moveX = _mainCamera.transform.right * (Input.GetAxis("Horizontal") * 1); // 左右(カメラ基準)
+        Vector3 moveDirection = moveX + moveZ;
 
         // 地面でスペースを押したらジャンプ
-        if (Input.GetKey(KeyCode.Space) && isOnGround) moveDirection.y = jump;
-        
-        
+        if (Input.GetKey(KeyCode.Space) && _isOnGround) moveDirection.y = jump;
+
+
         // 移動実行
-        if(rb.velocity.magnitude <= speed) rb.AddForce(moveDirection * 0.25f, ForceMode.VelocityChange);
-        
+        if (_rb.velocity.magnitude <= speed) _rb.AddForce(moveDirection * 0.25f, ForceMode.VelocityChange);
+
         // 視点移動
         transform.Rotate( // 体を左右
             new Vector2(
                 0,
                 Input.GetAxis("Mouse X") * mouseSensibilityHorizontal
             ), Space.World);
-        headBone.Rotate( // 頭を上下
+        _headBone.Rotate( // 頭を上下
             new Vector2(
                 Input.GetAxis("Mouse Y") * -mouseSensibilityVertical,
                 0
             ), Space.Self);
-        
+
         // 頭の角度を取得
         // localHeadAngle = headBone.eulerAngles.y - transform.eulerAngles.y;
         // localHeadAngle = localHeadAngle > 180f ? localHeadAngle - 360f : localHeadAngle;
     }
-    
+
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag.Contains("Ground"))
-        {
-            isOnGround = true;
-        }
+        if (other.gameObject.tag.Contains("Ground")) _isOnGround = true;
     }
 
     private void OnCollisionExit(Collision other)
     {
-        if (other.gameObject.tag.Contains("Ground"))
-        {
-            isOnGround = false;
-        }
+        if (other.gameObject.tag.Contains("Ground")) _isOnGround = false;
     }
 }
